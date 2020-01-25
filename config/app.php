5aad9a5740c90e0ed427396ecca5f24a5ebb436b
suraj <surajupadhyaa@gmail.com>
2020-01-26 05:06:24 +0530

    asser url

--- config/app.php
+++ config/app.php
@@ -42,10 +42,10 @@ return [
     'debug' => env('APP_DEBUG', false),
 
     /*
+    |--------------------------
     |--------------------------------------------------------------------------
+    |------------------------------------------------
     | Application URL
-    |--------------------------------------------------------------------------
-    |
     | This URL is used by the console to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
     | your application so that it is used when running Artisan tasks.
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'http://email-module.disc-in.com/public'),
 
     /*
     |--------------------------------------------------------------------------
