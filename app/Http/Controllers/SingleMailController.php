070945168bd5eab96f2d68a8dc11003d228336d6
suraj <surajupadhyaa@gmail.com>
2020-02-13 23:45:11 +0530

    google login

---
+++ app/Http/Controllers/SingleMailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SingleMail;
+use Illuminate\Http\Request;
+
+class SingleMailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SingleMail  $singleMail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SingleMail $singleMail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SingleMail  $singleMail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SingleMail $singleMail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SingleMail  $singleMail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SingleMail $singleMail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SingleMail  $singleMail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SingleMail $singleMail)
+    {
+        //
+    }
+}
