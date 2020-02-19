dbafc68acc134c6585697083b99e9fa4cde823e1
suraj <surajupadhyaa@gmail.com>
2020-02-19 20:49:48 +0530

    backup code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,20 +8,22 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.png">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <!-- CSS Files -->
+    <link href="{{ asset('assets') }}/css/material-dashboard.css?v=3.1.1" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="{{ asset('assets') }}/demo/demo.css" rel="stylesheet" />
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <!-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -31,14 +33,14 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                   
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
+                 
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                        
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -70,11 +72,129 @@
                     </ul>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </nav> -->
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white">
+  <div class="container">
+    <div class="navbar-wrapper">
+      <a class="navbar-brand" href="">{{ isset($title)?$title:'' }}</a>
     </div>
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="navbar-toggler-icon icon-bar"></span>
+      <span class="navbar-toggler-icon icon-bar"></span>
+      <span class="navbar-toggler-icon icon-bar"></span>
+    </button> -->
+    <div class="justify-content-end ">
+      <ul class="navbar-nav">
+        
+        <li class="nav-item{{ isset($activePage) && $activePage == 'register' ? ' active' : '' }}">
+          <a href="{{ route('register') }}" class="nav-link">
+            <i class="material-icons">person_add</i> {{ __('Register') }}
+          </a>
+        </li>
+        <li class="nav-item{{ isset($activePage) && $activePage == 'login' ? ' active' : '' }}">
+          <a href="{{ route('login') }}" class="nav-link">
+            <i class="material-icons">fingerprint</i> {{ __('Login') }}
+          </a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
+        <div class="wrapper wrapper-full-page">
+  <div class="page-header login-page header-filter" filter-color="black" style="background-image: url('{{ asset('assets') }}/img/login.jpg'); background-size: cover; background-position: top center;align-items: center;" data-color="purple">
+          
+            @yield('content')
+            <footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="https://www.creative-tim.com">
+                {{ __('Creative Tim') }}
+            </a>
+            </li>
+            <li>
+            <a href="https://creative-tim.com/presentation">
+                {{ __('About Us') }}
+            </a>
+            </li>
+            <li>
+            <a href="http://blog.creative-tim.com">
+                {{ __('Blog') }}
+            </a>
+            </li>
+            <li>
+            <a href="https://www.creative-tim.com/license">
+                {{ __('Licenses') }}
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a style="color:red !important" href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a style="color:red !important" href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
+        </div>
+    </div>
+</footer>
+    </div>
+
+    </div>
+
+</div>
+
+
+    
+        <!--   Core JS Files   -->
+        <script src="{{ asset('assets') }}/js/core/jquery.min.js"></script>
+        <script src="{{ asset('assets') }}/js/core/popper.min.js"></script>
+        <script src="{{ asset('assets') }}/js/core/bootstrap-material-design.min.js"></script>
+        <script src="{{ asset('assets') }}/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+        <!-- Plugin for the momentJs  -->
+        <script src="{{ asset('assets') }}/js/plugins/moment.min.js"></script>
+        <!--  Plugin for Sweet Alert -->
+        <script src="{{ asset('assets') }}/js/plugins/sweetalert2.js"></script>
+        <!-- Forms Validations Plugin -->
+        <script src="{{ asset('assets') }}/js/plugins/jquery.validate.min.js"></script>
+        <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+        <script src="{{ asset('assets') }}/js/plugins/jquery.bootstrap-wizard.js"></script>
+        <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+        <script src="{{ asset('assets') }}/js/plugins/bootstrap-selectpicker.js"></script>
+        <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+        <script src="{{ asset('assets') }}/js/plugins/bootstrap-datetimepicker.min.js"></script>
+        <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+        <script src="{{ asset('assets') }}/js/plugins/jquery.dataTables.min.js"></script>
+        <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+        <script src="{{ asset('assets') }}/js/plugins/bootstrap-tagsinput.js"></script>
+        <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+        <script src="{{ asset('assets') }}/js/plugins/jasny-bootstrap.min.js"></script>
+        <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+        <script src="{{ asset('assets') }}/js/plugins/fullcalendar.min.js"></script>
+        <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+        <script src="{{ asset('assets') }}/js/plugins/jquery-jvectormap.js"></script>
+        <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+        <script src="{{ asset('assets') }}/js/plugins/nouislider.min.js"></script>
+        <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+        <!-- Library for adding dinamically elements -->
+        <script src="{{ asset('assets') }}/js/plugins/arrive.min.js"></script>
+        <!--  Google Maps Plugin    -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE'"></script>
+        <!-- Chartist JS -->
+        <script src="{{ asset('assets') }}/js/plugins/chartist.min.js"></script>
+        <!--  Notifications Plugin    -->
+        <script src="{{ asset('assets') }}/js/plugins/bootstrap-notify.js"></script>
+        <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+        <script src="{{ asset('assets') }}/js/material-dashboard.js?v=2.1.1" type="text/javascript"></script>
+        <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+        <script src="{{ asset('assets') }}/demo/demo.js"></script>
+        <script src="{{ asset('assets') }}/js/settings.js"></script>
+        @stack('js')
 </body>
 </html>
